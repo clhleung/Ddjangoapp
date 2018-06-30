25f29f6c8a8611934154cb1f5f8b736a4e6c0cbe
clhleung <bobleung96@yahoo.com>
2018-06-30 15:13:31 -0700

    added desc of variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,3 +1,6 @@
+# variables.tf
+# Terraform file for all variables Terraform may use to create instances and security groups they may use
+
 variable "access_key" {default = "access key"}
 
 variable "secret_key"{default = "secret_key"}
