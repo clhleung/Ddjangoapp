b8ffcb2bbf0132f15bd90d124bef65ad894e53fe
clhleung <bobleung96@yahoo.com>
2018-06-30 15:19:24 -0700

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,14 +1,9 @@
 # variables.tf
 # Terraform file for all variables Terraform may use to create instances and security groups they may use
 
+variable "access_key" {default = "access_key"}
 
-
-variable "access_key" {default = "access key"}
-
-
-variable "access_key" {default = "AKIAIOJTT7TJOJHR44NQ"}
-
-variable "secret_key"{default = "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"}
+variable "secret_key"{default = "secret_key"}
 
 variable "region" {
   default = "us-east-2"
