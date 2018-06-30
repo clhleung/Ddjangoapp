636c1d415e2c0ca5870b1aa388eb436df92306d0
Clement Leung <clhleung@ucsc.edu>
2018-06-30 15:32:08 -0700

    remove private info

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,9 +1,9 @@
 # variables.tf
 # Terraform file for all variables Terraform may use to create instances and security groups they may use
 
-variable "access_key" {default = "access_key"}
+variable "access_key" {default = "access"}
 
-variable "secret_key"{default = "secret_key"}
+variable "secret_key"{default = "secret"}
 
 variable "region" {
   default = "us-east-2"
